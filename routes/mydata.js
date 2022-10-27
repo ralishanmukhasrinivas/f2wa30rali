7f0d2b30ddbcca2c7d0032237b613d1d0438f67b
ralishanmukhasrinivas <112181435+ralishanmukhasrinivas@users.noreply.github.com>
2022-10-26 21:20:03 -0500

    added

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var times=0
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('users access are :'+times);
-  times=times+1
+  res.render('mydata', { title: 'Shanmukha srinivas rali' });
 });
 
 module.exports = router;
